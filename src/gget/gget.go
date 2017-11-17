050b0cdd83a00248220ec39878c07b952d6cb258
Brian Jones <beeceej.code@gmail.com>
2017-11-17 17:15:31 -0600

    implement initial tests

--- src/gget/gget.go
+++ src/gget/gget.go
@@ -17,34 +17,30 @@ type retriever interface {
 
 type httpRetriever struct{}
 
-var r *httpRetriever
-
-func init() {
-	r = &httpRetriever{}
-}
-
-// Get will perform an HTTP GET on the specified URL and return the raw bytes
-// If you use this method, you do not need to supply a save.Saver strategy
-// and you may handle the file as you wish, use Execute if you wish to utilize a
-// prebuilt strategy for persisting your file.
+// get will perform an HTTP GET on the specified URL and return the raw bytes
+// this is an internal method which GGet relies on to download a file,
+// have another retrieval strategy? make a PR :)
+// The consumers of GGet shouldn't concern themselves with how it retrieves
+// unless they want to dig into the code
 func (r *httpRetriever) get(uri *url.URL) (b []byte, err error) {
 	return r.getFile(uri)
 }
 
 func (r *httpRetriever) getFile(url *url.URL) (b []byte, err error) {
 	var resp *http.Response
+	if url == nil {
+		return nil, fmt.Errorf("empty url, cannot retrieve a ghost resource")
+	}
 	if resp, err = http.Get(url.String()); err != nil {
 		return nil, fmt.Errorf("Error fetching %s: %v", url.String(), err.Error())
 	}
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("Error fetching %s. Error Code was: %d", url.String(), resp.StatusCode)
 	}
-
 	if b, err = ioutil.ReadAll(resp.Body); err != nil {
 		panic(fmt.Errorf("%s: %s", "Critical error reading response body", err.Error()))
 	}
 	defer resp.Body.Close()
-
 	return b, nil
 }
 
@@ -53,13 +49,21 @@ func (r *httpRetriever) getFile(url *url.URL) (b []byte, err error) {
 type GGet struct {
 	URL      *url.URL
 	Strategy strategy.Handler
+
+	r retriever
+}
+
+// Default returns an opinionated version of GGet
+// utilizing a retriever that defaults to an HTTP GET
+func Default(u *url.URL, s strategy.Handler) *GGet {
+	return &GGet{URL: u, Strategy: s, r: &httpRetriever{}}
 }
 
 // Execute will execute gget with the provided strategy
 func (g *GGet) Execute() (err error) {
 	var b []byte
 
-	if b, err = r.get(g.URL); err != nil {
+	if b, err = g.r.get(g.URL); err != nil {
 		return err
 	}
 
--- src/gget/gget_test.go
+++ src/gget/gget_test.go
@@ -1,22 +1,99 @@
 package gget
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
 	"net/url"
 	"testing"
+
+	"github.com/jofo8948/gget/src/strategy"
 )
 
-type mockGetter struct {
+const (
+	PASS = true
+	FAIL = false
+)
+
+func pass(msg string) bool {
+	return PASS
+}
+func fail(msg string) bool {
+	return FAIL
+}
+
+type mockRetriever struct {
 	mockData []byte
-	err      error
+	mockErr  error
 }
 
-func (mg *mockGetter) Get(u *url.URL) ([]byte, error) {
-	if mg.err != nil {
-		return nil, mg.err
+var mockURL, _ = url.Parse("http://google.com")
+
+var ggetExpectError = GGet{URL: nil, Strategy: &strategy.ToStdOut{}, r: &mockRetriever{mockData: nil, mockErr: errors.New("an error")}}
+var ggetExpectData = GGet{URL: mockURL, Strategy: &strategy.ToStdOut{}, r: &mockRetriever{mockData: []byte{0, 1, 1, 1}}}
+
+func (r *mockRetriever) get(u *url.URL) ([]byte, error) {
+	if r.mockErr != nil {
+		return nil, r.mockErr
 	}
-	return mg.mockData, nil
+	return r.mockData, nil
 }
 
-func testGGet(t *testing.T) {
-	mock := mockGetter{mockData: []byte{0, 1, 1, 1, 1}, err: nil}
+func TestGGet(t *testing.T) {
+	testGGetExpectError := func() bool {
+		err := ggetExpectError.Execute()
+		if err == nil {
+			return fail("testGGetExpectError")
+		}
+
+		return pass("testGGetExpectError")
+	}
+	testGGetExpectNoError := func() bool {
+		if err := ggetExpectData.Execute(); err != nil {
+			fmt.Println(err.Error())
+			return fail("testGGetExpectNoError")
+		}
+		return pass("testGGetExpectNoError")
+	}
+
+	t1Status := testGGetExpectError()
+	checkTestStatus(t1Status, t)
+	t2Status := testGGetExpectNoError()
+	checkTestStatus(t2Status, t)
+}
+
+func TestHttpRetriever(t *testing.T) {
+	expectedResponse := []byte{0, 1, 1}
+	tstServer := start([]byte{0, 1, 1})
+	defer tstServer.Close()
+
+	localhostURL, _ := url.Parse("http://localhost:8080/test")
+	if response, err := new(httpRetriever).get(localhostURL); err != nil {
+		t.Fail()
+	} else if !bytes.Equal(response, expectedResponse) {
+		t.Fail()
+	}
+}
+
+func checkTestStatus(status bool, t *testing.T) {
+	if status != PASS {
+		t.Fail()
+	}
+
+}
+
+func start(expectedResponse []byte) *http.Server {
+	server := &http.Server{Addr: ":8080"}
+	http.HandleFunc("/test", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write(expectedResponse)
+	})
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("Server errord out: %s", err)
+		}
+	}()
+
+	return server
 }
