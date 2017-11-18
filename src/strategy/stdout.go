5b0b222558a11325e3a3aa1c1b04c5943df05da3
Brian Jones <beeceej.code@gmail.com>
2017-11-18 17:25:39 -0600

    add size as a field in ToStdOut

--- src/strategy/stdout.go
+++ src/strategy/stdout.go
@@ -3,10 +3,17 @@ package strategy
 import "log"
 
 // ToStdOut provides a strategy for printing file contents to std out
-type ToStdOut struct{}
+type ToStdOut struct {
+	Size int
+}
 
 // Handle is a strategy for printing file contents out to std out
 func (t *ToStdOut) Handle(b []byte) error {
-	log.Printf(string(b))
+	if t.Size != 0 {
+		log.Printf(string(b)[:t.Size])
+	} else {
+		log.Printf(string(b))
+	}
+
 	return nil
 }
